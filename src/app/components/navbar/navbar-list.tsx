a0b48421f638c82ed9d497b119307dec4d0e0757
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-08-29 23:00:48 +0700

    chore: remove unused code

--- src/app/components/navbar/navbar-list.tsx
+++ src/app/components/navbar/navbar-list.tsx
@@ -1,5 +1,4 @@
 import {
-  CartIcon,
   CartListIcon,
   MenuIcon,
   NextIcon,
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Navbar } from '../components/navbar/navbar'
 
 function Page() {
   return <h1>Halaman Profile</h1>
